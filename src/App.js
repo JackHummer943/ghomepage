ad5a7e8b2e13585a12d5dd89fd3b95c25593e666
evg_ser1997@mail.ru <evg_ser1997@mail.ru>
2023-02-03 17:56:05 +0300

    0.75

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="html">
+      <div className="wrapper">
+
+{/* аналог layout svelte-sgx7pv*/}
+<div className="headerLeft">
+
+<div className="low">      
+<div className="autograph"  >
+  <a className="svetle-18" arial-hidden="true">
+  <svg className="svelte-qivg81" xmlns="http://www.w3.org/2000/svg"></svg>
+  </a>
+</div>
+<nav>
+  <ul className="svelte-dmnv9n">
+    <li className="svelte-dmnv9n">
+      <a className="svelte-dmnv9n">Work</a> 
+      </li>
+    <li className="svelte-dmnv9n">
+        <a className="svelte-dmnv9n" href="/about">About</a> 
+        </li>
+      <li className="svelte-dmnv9n">
+          <a className="svelte-dmnv9n" href="/contact">Contact</a>
+           </li>
+           </ul>
+           </nav>
+           </div>
+</div>
+
+<main>
+  <ul>
+    <li>Dry Media</li>
+    <li>Ink</li>
+    <li>Colour</li>
+    <li>Pastel</li>
+    <li>Watercolour</li>
+  </ul>
+</main>
+<footer>
+  <p>
+  {/* ©&nbsp;
+  2011
+  –
+  2023 */}
+  </p>
+</footer>
+
+</div>
     </div>
   );
 }
