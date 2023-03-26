fa813460b640ab663d7709a26e144f6002b3d48e
evg_ser1997@mail.ru <evg_ser1997@mail.ru>
2023-03-26 17:50:05 +0300

    message1

---
+++ src/pages/Work.jsx
@@ -0,0 +1,29 @@
+function Work() {
+  return(  <div className="work">
+  <main className="cover svelte-dkfo2e">
+<ul className="svelte-14b3obz">
+<li className="svelte-14b3obz">
+<a  className="svelte-9k6p9w" >
+Dry Media</a> 
+
+</li>
+<li className="svelte-14b3obz">
+<a href="/ink" className="svelte-9k6p9w" >
+Ink</a>
+
+</li>
+<li className="svelte-14b3obz">
+<a href="/colour-pastel" className="svelte-9k6p9w">
+Colour Pastel</a>
+   </li>
+   <li className="svelte-14b3obz">
+<a href="/colour-pastel" className="svelte-9k6p9w">
+Watercolour</a>
+   </li>
+   
+         </ul>
+         </main> 
+</div>);
+}
+
+export default Work;
