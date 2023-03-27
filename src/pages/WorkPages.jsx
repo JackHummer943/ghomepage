cf671d9d9ad51fabc94c549a6cd2242b75d77d55
evg_ser1997@mail.ru <evg_ser1997@mail.ru>
2023-03-27 20:43:08 +0300

    version 0.67

---
+++ src/pages/WorkPages.jsx
@@ -0,0 +1,11 @@
+
+function WorkPages() {
+return(  <>
+<li className="svelte-dmnv9n">
+  
+<a  href="/dry_media" className="svelte-dmnv9n">
+  
+  Dry Media</a> 
+</li></>);
+}
+export default WorkPages;
