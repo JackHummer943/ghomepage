9226240b3bbe6e1e65c597796df0f10dc3f172f8
evg_ser1997@mail.ru <evg_ser1997@mail.ru>
2023-03-30 23:07:54 +0300

    0,75

--- src/pages/WorkPages.jsx
+++ src/pages/WorkPages.jsx
@@ -1,11 +1,15 @@
-
+import kihot from '../assets/kihot.png';
+import pansa from '../assets/pansa.png';
+<assets />
 function WorkPages() {
 return(  <>
 <li className="svelte-dmnv9n">
   
 <a  href="/dry_media" className="svelte-dmnv9n">
   
-  Dry Media</a> 
+ <img src={kihot} style={{marginTop: 40 + '%'}}alt=""/>
+ <img src={pansa} alt=""/>
+ </a> 
 </li></>);
 }
 export default WorkPages;
