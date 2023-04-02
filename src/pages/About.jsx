659fb5fee2ddd6ffefeafdb0dfd93ae357ed3912
evg_ser1997@mail.ru <evg_ser1997@mail.ru>
2023-04-02 23:58:01 +0300

    version09

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,23 @@
 import logo from '../assets/pizza-logo.png';
+import aboutPhoto from '../assets/About.jpg';
 
 function About() {
   return(
-  <>
-   <div className="block portrait">
-      <img src={logo} className="portrait_img"/>
-      </div> 
+  <div className="block_portrait">
+      <div className="child1">
+      <img src={aboutPhoto} className="portrait_img"/>
+      </div>
       <div className="description">
-        <p className="description">G Dacksa is an acclaimed graphic artist .</p>
+        <p className="description">G Dacksa is an acclaimed graphic artist and pedagogue. 
+        Known at home primarily by illustration work, with over 70 books spanning four decades, 
+        he is better recognised abroad for portraiture, 
+        nudes and landscapes featured in museums and private galleries.
+Merited Artist of Russia and Honorary member of Russian Academy of Fine Arts, 
+Klim graduated from St. Petersburg Ilya Repin Institute in 1976 and never left, 
+having since served his alma mater in every capacity from faculty assistant to the dean of Graphic arts,
+ and, currently, a supervising professor of the book arts programme..</p>
 </div>
-  </>
+  </div>
   );
 }
 export default About;
