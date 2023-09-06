e60b337e4a1bcc9eca9dcaa8103188970778b32f
evg_ser1997@mail.ru <evg_ser1997@mail.ru>
2023-09-06 20:38:39 +0300

    выравнивание меню по высоте

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,67 +1,59 @@
 import { hover } from '@testing-library/user-event/dist/hover';
 import logo from '../assets/mainTitle.jpg';
-function Home(){
-return(
-  <div className="home">
-   
-      
-      
-      <header>
+import '../../src/home.css';
 
+function Home() {
+  return (
+    <div className="home">
+      <div className="left">
+        <ul className="home_block">
+          <li>
+            <a href="/work">work</a>
+          </li>
+          <li>
+            <a href="/about">about</a>
+          </li>
+          <li>
+            <a href="/contact">contact</a>
+          </li>
+        </ul>
+      </div>
 
+      <div className="rigth">
+        <nav>
+          {' '}
+          <ul>
+            <li className="svelte-dmnv9n">
+              <a href="/dry_media" className="svelte-dmnv9n">
+                Illustrations
+              </a>
+            </li>
 
+            <li className="svelte-dmnv9n">
+              <a href="/about" className="svelte-dmnv9n">
+                Comics
+              </a>
+            </li>
+            <li className="svelte-dmnv9n">
+              <a href="/contact" className="svelte-dmnv9n">
+                Printings
+              </a>
+            </li>
+            <li className="svelte-dmnv9n">
+              <a href="/contact" className="svelte-dmnv9n">
+                Motion
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
 
-<ul className="home_block">
-
-<li >
-
-<a  href="/work" >work</a> 
-</li>
-<li >
-<a href="/about" >about</a>
-</li>
-<li >
-<a href="/contact" >contact</a>
-</li>
-</ul>
-  
-
-
-
-
-
- 
- </header>
-
-  
-<nav>  <ul className="svelte-dmnv9n">
-  <li className="svelte-dmnv9n">
-  
-  <a  href="/dry_media" className="svelte-dmnv9n">
-    
-  Illustrations
-   </a> 
-  </li>
-
-<li className="svelte-dmnv9n">
-<a href="/about" className="svelte-dmnv9n" >Comics</a>
- </li>
-<li className="svelte-dmnv9n">
-<a href="/contact" className="svelte-dmnv9n">Printings</a>
- </li>
- <li className="svelte-dmnv9n">
-<a href="/contact" className="svelte-dmnv9n" >Motion</a>
- </li>
-
- </ul></nav>
- 
-
- <footer className="svelte-12ieuf3"><p> (c) 2023 
-     G.Dacksa</p>
-</footer>
+      <footer className="footer">
+        <p> (c) 2023 G.Dacksa</p>
+      </footer>
     </div>
-)
-};
+  );
+}
 // window.scrollTo({top: 0});
 
 export default Home;
