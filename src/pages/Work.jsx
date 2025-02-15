79b1d99f17bc30757ceda8849341d98e074ab10e
devJ <bessonov9751@gmail.com>
2025-02-15 22:50:51 +0300

    исправление страниц с работами

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -32,7 +32,7 @@ function Work() {
   // }, []);
 
   return (
-    <div className="work" style={{ width: 'fit-content' }}>
+    <>
       <div className="left_block">
         <Link to="/">
           <div className="emblem">
@@ -46,7 +46,7 @@ function Work() {
                 marginTop: '200px',
                 marginBottom: '10px',
               }}
-            />{' '}
+            />
           </div>
         </Link>
         <ul className="list">
@@ -59,119 +59,121 @@ function Work() {
           </li>
         </ul>
       </div>
-      <article className="image_block">
-        {images.map((obj) => (
-          // создан отдельный компонент для изображений, чтобы вынести все стили
-          // вопрос, брать изображения из БД или оставить изображения внутри проекта?
-          <ImageElem key={obj.id} {...obj} img={obj.imageUrl} />
-        ))}
-        <section className="image_block_wrapper">
-          <img
-            src={kihot}
-            alt=""
-            decoding="async"
-            className="svelte-efm2wi"
-            style={{ width: '700px', marginBottom: '100px' }}
-          />{' '}
-        </section>
-        <section className="image_block_wrapper">
-          <img
-            src={pansa}
-            alt=""
-            decoding="async"
-            className="svelte-efm2wi"
-            style={{ width: '700px', marginBottom: '100px' }}
-          />{' '}
-        </section>
+      <div className="work" style={{ width: 'fit-content' }}>
+        <article className="image_block">
+          {images.map((obj) => (
+            // создан отдельный компонент для изображений, чтобы вынести все стили
+            // вопрос, брать изображения из БД или оставить изображения внутри проекта?
+            <ImageElem key={obj.id} {...obj} img={obj.imageUrl} />
+          ))}
+          <section className="image_block_wrapper">
+            <img
+              src={kihot}
+              alt=""
+              decoding="async"
+              className="svelte-efm2wi"
+              style={{ width: '700px', marginBottom: '100px' }}
+            />
+          </section>
+          <section className="image_block_wrapper">
+            <img
+              src={pansa}
+              alt=""
+              decoding="async"
+              className="svelte-efm2wi"
+              style={{ width: '700px', marginBottom: '100px' }}
+            />
+          </section>
 
-        <section className="image_block_wrapper">
-          <img
-            src={pictureTwo}
-            alt=""
-            decoding="async"
-            className="svelte-efm2wi"
-            style={{ width: '700px', marginBottom: '100px' }}
-          />{' '}
-        </section>
-        <section className="image_block_wrapper">
-          <img
-            src={pictureThree}
-            alt=""
-            decoding="async"
-            className="svelte-efm2wi"
-            style={{ width: '700px', marginBottom: '100px' }}
-          />{' '}
-        </section>
-        <section className="image_block_wrapper">
-          <img
-            src={pictureFour}
-            alt=""
-            decoding="async"
-            className="svelte-efm2wi"
-            style={{ width: '700px', marginBottom: '100px' }}
-          />{' '}
-        </section>
-        <section className="image_block_wrapper">
-          <img
-            src={pictureFive}
-            alt=""
-            decoding="async"
-            className="svelte-efm2wi"
-            style={{ width: '700px', marginBottom: '100px' }}
-          />{' '}
-        </section>
+          <section className="image_block_wrapper">
+            <img
+              src={pictureTwo}
+              alt=""
+              decoding="async"
+              className="svelte-efm2wi"
+              style={{ width: '700px', marginBottom: '100px' }}
+            />
+          </section>
+          <section className="image_block_wrapper">
+            <img
+              src={pictureThree}
+              alt=""
+              decoding="async"
+              className="svelte-efm2wi"
+              style={{ width: '700px', marginBottom: '100px' }}
+            />
+          </section>
+          <section className="image_block_wrapper">
+            <img
+              src={pictureFour}
+              alt=""
+              decoding="async"
+              className="svelte-efm2wi"
+              style={{ width: '700px', marginBottom: '100px' }}
+            />
+          </section>
+          {/* <section className="image_block_wrapper">
+            <img
+              src={pictureFive}
+              alt=""
+              decoding="async"
+              className="svelte-efm2wi"
+              style={{ width: '700px', marginBottom: '100px' }}
+            />
+          </section>
 
-        <section className="image_block_wrapper">
-          <img
-            src={pictureSeven}
-            alt=""
-            decoding="async"
-            className="svelte-efm2wi"
-            style={{ width: '700px', marginBottom: '100px' }}
-          />{' '}
-        </section>
+          <section className="image_block_wrapper">
+            <img
+              src={pictureSeven}
+              alt=""
+              decoding="async"
+              className="svelte-efm2wi"
+              style={{ width: '700px', marginBottom: '100px' }}
+            />
+          </section> */}
 
-        <section className="image_block_wrapper">
-          <img
-            src={pictureTen}
-            alt=""
-            decoding="async"
-            className="svelte-efm2wi"
-            style={{ width: '700px', marginBottom: '100px' }}
-          />{' '}
-        </section>
-        <section className="image_block_wrapper">
-          <img
-            src={pictureEleven}
-            alt=""
-            decoding="async"
-            className="svelte-efm2wi"
-            style={{ width: '700px', marginBottom: '100px' }}
-          />{' '}
-        </section>
-        <section className="image_block_wrapper">
-          <img
-            src={pictureTwelve}
-            alt=""
-            decoding="async"
-            className="svelte-efm2wi"
-            style={{ width: '700px', marginBottom: '100px' }}
-          />{' '}
-        </section>
-        <section className="image_block_wrapper">
-          <img
-            src={pictureThirdTe}
-            alt=""
-            decoding="async"
-            className="svelte-efm2wi"
-            style={{ width: '700px', marginBottom: '100px' }}
-          />{' '}
-        </section>
-      </article>
-      <footer className="scroll">
-        <p> (c) 2023 G.Dacksa</p>
-      </footer>
-    </div>
+          <section className="image_block_wrapper">
+            <img
+              src={pictureTen}
+              alt=""
+              decoding="async"
+              className="svelte-efm2wi"
+              style={{ width: '700px', marginBottom: '100px' }}
+            />
+          </section>
+          <section className="image_block_wrapper">
+            <img
+              src={pictureEleven}
+              alt=""
+              decoding="async"
+              className="svelte-efm2wi"
+              style={{ width: '700px', marginBottom: '100px' }}
+            />
+          </section>
+          <section className="image_block_wrapper">
+            <img
+              src={pictureTwelve}
+              alt=""
+              decoding="async"
+              className="svelte-efm2wi"
+              style={{ width: '700px', marginBottom: '100px' }}
+            />
+          </section>
+          <section className="image_block_wrapper">
+            <img
+              src={pictureThirdTe}
+              alt=""
+              decoding="async"
+              className="svelte-efm2wi"
+              style={{ width: '700px', marginBottom: '100px' }}
+            />
+          </section>
+        </article>
+        <footer className="scroll">
+          <p> (c) 2023 G.Dacksa</p>
+        </footer>
+      </div>
+    </>
   );
 }
 
